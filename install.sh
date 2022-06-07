8dcf72733ba91fc0af82ab249d85b9c899e6ee9c
Owen Rempel <owenrempel@gmail.com>
2022-06-07 09:56:50 -0600

    DOing some debugging

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
-apt update -y && apt upgrade -y
+#apt update -y && apt upgrade -y
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
-dpkg -i google-chrome-stable_current_amd64.deb
+#dpkg -i google-chrome-stable_current_amd64.deb
 
-apt --fix-broken install -y
+#apt --fix-broken install -y
 
-mkdir RouterReboot
+#mkdir RouterReboot
 
-cd RouterReboot
+#cd RouterReboot
 
 chrome_driver=$(curl "https://chromedriver.storage.googleapis.com/LATEST_RELEASE") 
 
 curl -Lo chromedriver_linux64.zip "https://chromedriver.storage.googleapis.com/${chrome_driver}/chromedriver_linux64.zip"
 
-mkdir -p "chromedriver/stable" && \
-unzip -q "chromedriver_linux64.zip" -d "chromedriver/stable" && \
-chmod +x "chromedriver/stable/chromedriver"
+#mkdir -p "chromedriver/stable" && \
+#unzip -q "chromedriver_linux64.zip" -d "chromedriver/stable" && \
+#chmod +x "chromedriver/stable/chromedriver"
 
-apt install python3 python3-pip -y
+#apt install python3 python3-pip -y
 
-pip3 install selenium
+#pip3 install selenium
 
-wget https://raw.githubusercontent.com/OwenRempel/TelusZTE-Restart/master/main.py
+#wget https://raw.githubusercontent.com/OwenRempel/TelusZTE-Restart/master/main.py
