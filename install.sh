6417b25177a6aea6932aacff08de2558a75ac58d
Owen Rempel <owenrempel@gmail.com>
2022-06-08 08:26:22 -0700

    Added -qq to all the apt commands and added echo statments

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-apt update -y && apt upgrade -y
+echo 'Updating system'
+apt update -qq -y && apt upgrade -qq -y
 
-apt install curl unzip python3 python3-pip -y
+echo 'Installing required packages'
+apt install curl unzip python3 python3-pip -qq -y
 
+echo 'Downloading Chrome'
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 dpkg -i google-chrome-stable_current_amd64.deb
 
-apt --fix-broken install -y
+apt --fix-broken install -qq -y
 
 mkdir RouterReboot
 
@@ -16,14 +19,15 @@ cd RouterReboot
 
 chrome_driver=$(curl "https://chromedriver.storage.googleapis.com/LATEST_RELEASE") 
 
+echo 'Downloading ChromeDriver'
 curl -Lo chromedriver_linux64.zip "https://chromedriver.storage.googleapis.com/${chrome_driver}/chromedriver_linux64.zip"
 
 mkdir -p "chromedriver/stable" && \
 unzip -q "chromedriver_linux64.zip" -d "chromedriver/stable" && \
 chmod +x "chromedriver/stable/chromedriver"
 
-
-
+echo 'Installing Selenium'
 pip3 install selenium
 
+echo 'Downloading Main Script'
 wget https://raw.githubusercontent.com/OwenRempel/TelusZTE-Restart/master/main.py
